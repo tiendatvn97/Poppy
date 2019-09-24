0d5b8fa4be13a2cf033582410ed4a99a72cdf7a1
tiendat <tiendatvn97>
2019-09-25 01:42:33 +0700

    add drawNavigation

--- App.js
+++ App.js
@@ -1,55 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ScreenManager from "./src/screens/ScreenManager"
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ScreenManager from "./src/screens/ScreenManager";
 import { Provider } from "mobx-react/native";
-import config from "./src/mobx"
+import config from "./src/mobx";
 
-import * as Font from 'expo-font'
-import { AppLoading } from 'expo';
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 
-import LoginView from "./src/views/Login/LoginView"
-import SingInView from "./src/views/Login/SignInView"
-import RegisterView from "./src/views/Login/RegisterView"
-import NewsFeedView from "./src/views/Feed/NewsFeedView"
+import LoginView from "./src/views/Login/LoginView";
+import SingInView from "./src/views/Login/SignInView";
+import RegisterView from "./src/views/Login/RegisterView";
+import NewsFeedView from "./src/views/Feed/NewsFeedView";
 
-// export default function App() {
-//   const stores = config();
-//   return (
-//     <Provider {...stores}>
-//       <ScreenManager/>
-//     </Provider>
-//   );
-// }
-
-export default class App extends React.Component {
-  constructor(props) {
-    console.disableYellowBox = true;
-    super(props);
-    this.state = { loading: true };
-  }
-  async componentWillMount() {
-    await Font.loadAsync({
-      Roboto: require("./resources/Roboto.ttf"),
-      Roboto_medium: require("./resources/Roboto_medium.ttf"),
-      Ionicons: require("./resources/Ionicons.ttf"),
-    });
-    this.setState({ loading: false });
-  }
-
-  render(){
-    if (this.state.loading) {
-      return <AppLoading/>
-    }
-    return(
-      <NewsFeedView/>
-    )
-  }
+export default function App() {
+  const stores = config();
+  return (
+<Provider {...stores}>
+  <ScreenManager/>
+</Provider>
+  );
 }
+
+// export default class App extends React.Component {
+//   constructor(props) {
+//     const stores= config();
+//     console.disableYellowBox = true;
+//     super(props);
+//     this.state = { loading: true };
+//   }
+//   async componentWillMount() {
+//     await Font.loadAsync({
+//       Roboto: require("./resources/Roboto.ttf"),
+//       Roboto_medium: require("./resources/Roboto_medium.ttf"),
+//       Ionicons: require("./resources/Ionicons.ttf")
+//     });
+//     this.setState({ loading: false });
+//   }
+
+//   render() {
+//     if (this.state.loading) {
+//       return <AppLoading />;
+//     }
+//     return (
+//       // <NewsFeedView/>
+//       <Provider {...stores}>
+//         <ScreenManager />
+//       </Provider>
+//     );
+//   }
+// }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- src/screens/ScreenManager.js
+++ src/screens/ScreenManager.js
@@ -1,11 +1,73 @@
-import { createAppContainer } from "react-navigation"
-import { createStackNavigator } from 'react-navigation-stack';
+import React from "react";
+import { ScrollView, SafeAreaView, Image } from "react-native";
 import MainScreen from "./MainScreen";
 import SecondScreen from "./SecondScreen";
+import { StyleSheet, StatusBar } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerNavigatorItems
+} from "react-navigation-drawer";
+import { createAppContainer } from "react-navigation";
 
-const stackNavigation = createStackNavigator({
-    Main: MainScreen,
-    Second: SecondScreen
-})
+import {
+  View,
+  Container,
+  Text,
+  Header,
+  Content,
+  // Icon,
+  Body,
+  Left
+} from "native-base";
 
-export default  appContainer = createAppContainer(stackNavigation);
+const CustomDrawerContentComponent = props => (
+  <Container>
+    <Header style={styles.headerContainer}>
+      <Body style={{ alignItems: "center" }}>
+        <Image
+          style={styles.headerImage}
+          source={require("../icons/avatar.jpg")}
+        ></Image>
+      </Body>
+    </Header>
+    <Content>
+      <DrawerNavigatorItems {...props} />
+    </Content>
+  </Container>
+);
+
+let routeConfigs = {
+  Main: { screen: MainScreen },
+  Second: { screen: SecondScreen }
+};
+let drawerNavigationConfig = {
+  initialRouteName: "Main",
+  contentComponent: CustomDrawerContentComponent,
+  contentOptions: {
+    // activeTintColor: "orange".
+    // drawerWidth: 150,
+    // drawerPosition: "left",
+  },
+  // order:[Home, Setting]
+  drawerPosition: "left"
+  // order: ["Setting", "Home"]
+};
+const drawerNavigator = createDrawerNavigator(
+  routeConfigs,
+  drawerNavigationConfig
+);
+export default createAppContainer(drawerNavigator);
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 200,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: StatusBar.currentHeight
+  },
+  headerImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75
+  }
+});
